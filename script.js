9e5b69d482eb780884ea2d7b3441a95564e7dd09
kush7588 <kushbaid10@gmail.com>
2025-02-27 18:32:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".interactive-area").addEventListener("click", function() {
+        const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = randomColor;
+    });
+});
